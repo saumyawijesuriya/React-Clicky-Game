d9b4ae3901747971ea093de9a4ebfa5b83571b01
Saumya Wijesuriya <Saumyawijesuriya@yahoo.com>
2019-11-17 13:49:18 -0500

    working on Navbar component

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = props => <div className = "clicky-nav">
+        <div className= "nav-title">
+<h1 id="status">{props.status}</h1>
+<a className ="nav-restart" href="/">Restart</a>
+
+<div className= "nav-scores">
+        <div className="score" > | Top Score: {props.topScore}</div>
+        <div className="score" >Current Score: {props.currentScore}</div>
+</div>
+        </div>
+</div>
+
+export default Navbar;
